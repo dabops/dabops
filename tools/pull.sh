8025bf43e83a60d856d9641de396d9228510d106
Fabio.ROCCO <fabio.rocco@green-conseil.com>
2019-03-11 15:58:39 +0100

    Tools: Update the script for pull repositories

--- tools/pull.sh
+++ tools/pull.sh
@@ -4,17 +4,17 @@ BASEDIR=$(dirname "$0")
 cd $BASEDIR
 
 pathfile="../repositories.txt"
-repositories=()
 
 cd ../microservices
-while read REPO
+
+while IFS="" read -r REPO || [ -n "$REPO" ]
 do
   if [ -d $REPO ]
-  	then
-  	    cd $REPO
-  	    git pull origin master
-  	    cd ..
-  	else
-  	    git clone https://github.com/dabops/"$REPO".git
-  	fi
+    then
+      cd $REPO
+      git pull origin master
+      cd ..
+    else
+      git clone https://github.com/dabops/"$REPO".git
+    fi
 done < "$pathfile"
