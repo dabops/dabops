c34d8f7b1326508555dd4005c0053aece764b9eb
Rubie De Oliveira <rubiedeoliveira@gmail.com>
2019-03-16 15:49:32 +0100

    pull.sh updated, including now wiki

--- tools/pull.sh
+++ tools/pull.sh
@@ -13,8 +13,22 @@ do
     then
       cd $REPO
       git pull origin master
+      # IF WIKI repot exists
+      if [ -d "wiki" ]
+          then
+            cd "wiki"
+            git pull origin master
+            cd ..
+          else
+            git clone https://github.com/dabops/"$REPO".wiki.git
+            mv "$REPO".wiki wiki
+      fi
       cd ..
     else
       git clone https://github.com/dabops/"$REPO".git
+      cd $REPO
+      git clone https://github.com/dabops/"$REPO".wiki.git
+      mv "$REPO".wiki wiki
+      cd ..
     fi
 done < "$pathfile"
